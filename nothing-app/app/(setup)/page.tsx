8c8a911904d7cf6e164f3007741b373dd240f4e5
00Manas-Singh00 <110744893+00Manas-Singh00@users.noreply.github.com>
2024-01-28 02:47:00 +0530

    changes made in initial-modal.tsx file

--- nothing-app/app/(setup)/page.tsx
+++ nothing-app/app/(setup)/page.tsx
@@ -1,5 +1,6 @@
 import {initialProfile} from "@/lib/initial-profile";
 import {db} from "@/lib/db"
+import { InitialModal } from "@/components/modals/initial-modal"
 
 import {redirect} from "next/navigation"
 
@@ -17,10 +18,10 @@ const server = await db.server.findFirst({
 })
 
 if(server){
-    return redirect
+    return redirect(`/servers/${server.id}`)
 }
 
-    return <div>Create a server</div>
+    return <InitialModal/>
 }
 
 export default SetupPage;
