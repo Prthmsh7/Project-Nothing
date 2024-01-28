d6c3948f9e21d182050e1a071e97c161d091e25a
00Manas-Singh00 <110744893+00Manas-Singh00@users.noreply.github.com>
2024-01-28 15:01:40 +0530

    modified initial modal

--- nothing-app/components/modals/initial-modal.tsx
+++ nothing-app/components/modals/initial-modal.tsx
@@ -4,6 +4,7 @@ import * as z from "zod";
 
 import { zodResolver } from '@hookform/resolvers/zod';
 import { object, string } from 'zod';
+import {useForm} from "react-hook-form";
 
 
 
@@ -15,7 +16,17 @@ import {
     DialogTitle,
     DialogContent
 } from "@/components/ui/dialog"
-import {useForm} from "react-hook-form";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage
+} from "@/components/ui/form"
+import  {Input} from "@/components/ui/input"
+import {Button} from "@/components/ui/button"
 
 export const InitialModal = () => {
 
@@ -38,6 +49,37 @@ export const InitialModal = () => {
                         Give your classroom a personality with a name and an image. You can always chage it later
                     </DialogDescription>
                 </DialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <div className="space-y-8 px-6">
+                            <div className="flex items-center justify-center text-center">
+                                TODO: Image upload
+                            </div>
+                            <FormField control={form.control} name="name" render={({ field}) => {
+                                <FormItem>
+                                    <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                                        Class Name
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            disabled={isLoading}
+                                            className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                                            placeholder="Enter class name"
+                                            {...field}
+                                        />
+                                        
+                                    </FormControl>
+                                </FormItem>
+                            }}/>
+                        </div>
+                        <DialogFooter className="bg-gray-100 px-6 py-4">
+                            <Button variant="primary" disabled={isLoading}>
+                                Create
+                            </Button>
+
+                        </DialogFooter>
+                    </form>
+                </Form>
             </DialogContent>
         </Dialog>
     )
