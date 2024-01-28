2d74ee617e930460e897bb88a0349b4c19f7163f
00Manas-Singh00 <110744893+00Manas-Singh00@users.noreply.github.com>
2024-01-28 23:19:30 +0530

    completed

--- nothing-app/components/navigation/navigation-action.tsx
+++ nothing-app/components/navigation/navigation-action.tsx
@@ -3,8 +3,11 @@
 import {Plus} from "lucide-react"
 
 import { ActionTooltip } from "@/components/action-tooltip"
+import { useModal } from "@/hooks/use-modal-store";
 
 export const NavigationAction = () => {
+    const {onOpen} = useModal();
+
   return(
     <div>
         <ActionTooltip
@@ -12,9 +15,11 @@ export const NavigationAction = () => {
         align="center"
         label="Add a classroom"
         >
-        <button className="group flex items-center">
+        <button 
+        onClick={() => onOpen("createServer")}
+        className="group flex items-center">
             <div className="flex mx-3 p-2 rounded-[24px] group-hover:rounded-[24px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-red-500">
-                <Plus className="group-hover:text-white transition text-yellow-500" size={25}/>
+                <Plus className="group-hover:text-white transition text-red-500" size={25}/>
             </div>
         </button>
         </ActionTooltip>
