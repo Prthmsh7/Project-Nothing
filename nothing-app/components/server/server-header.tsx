e41d5dce5506dc4547ff24b09d92608d88ca8b9c
ayushmati2004 <ayushmati4@gmail.com>
2024-02-04 07:26:12 +0530

    manage members

--- nothing-app/components/server/server-header.tsx
+++ nothing-app/components/server/server-header.tsx
@@ -46,6 +46,7 @@ interface ServerHeaderProps{
                 )}
                 {isAdmin && (
                         <DropdownMenuItem
+                        onClick={() => onOpen("members",{server})}
                         className=" px-3 py-2 text-sm cursor-pointer">
                             Manage Members
                             <Users className="h-4 w-4 ml-auto"/>
